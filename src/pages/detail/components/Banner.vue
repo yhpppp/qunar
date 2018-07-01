7a3db685ab06112b2b1f5c020ecfc1f796894e7c
yhpp <yhpp1995@gmail.com>
2018-07-01 22:24:46 +0800

    finish banner

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -1,20 +1,43 @@
 <template>
-  <div class="banner">
-    <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" alt="">
-     <div class="banner-info">
-        <div class="banner-tittle">
-          大连圣亚海洋世界(AAAA景区)
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" alt="">
+      <div class="banner-info">
+          <div class="banner-tittle">
+            大连圣亚海洋世界(AAAA景区)
+          </div>
+          <div class="banner-number">
+            <span class="iconfont banner-icon">&#xe63f;</span>
+            39
+          </div>
         </div>
-        <div class="banner-number">
-          <span class="iconfont banner-icon">&#xe63f;</span>
-          39
-        </div>
-      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="shwoGallary" @close="handleGallaryClose"></common-gallary>
   </div>
 </template>
 
 <script>
-export default {};
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+  name:'DetailBanner',
+  components: {
+    CommonGallary
+  },
+  data() {
+    return {
+      imgs: ['http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg','http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png'],
+      shwoGallary: false
+    }
+  },
+  methods: {
+    handleBannerClick() {
+      this.shwoGallary = true
+    },
+    handleGallaryClose() {
+      this.shwoGallary = false
+    }
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
